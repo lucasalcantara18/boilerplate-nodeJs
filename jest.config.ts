f7bc4b47668186987523ca1e348a57f12afd791d
Lucas Alcântara <lucasalcantara18@gmail.com>
2022-04-12 05:48:49 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+export default {
+  roots: ['<rootDir>/src'],
+  // Automatically clear mock calls, instances and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+
+  // The test environment that will be used for testing
+  // testEnvironment: "jest-environment-node",
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+
+};
